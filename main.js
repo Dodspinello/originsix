597c5c7586a94a4c773205350b6c87a84fe9477d
Dodspinello <adolfospinello@gmail.com>
2023-07-17 12:23:38 -0300

    active js menu

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ for(const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-
-function changeHeaderWhenScroll(){
     const header = document.querySelector('#header')
     const navHeight = header.offsetHeight
 
+    function changeHeaderWhenScroll(){
+    
     if(this.window.scrollY >= navHeight) {
         //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -67,9 +67,9 @@ footer .band, footer .social
 `, { interval: 100 })
 
 /// botão voltar para o topo
-function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
     if (window.scrollY >= 560) {
         backToTopButton.classList.add('show')
     }else {
@@ -77,8 +77,35 @@ function backToTop() {
     }
 }
 
+/// menu ativo conforme a seção visivel na pagina 
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for( const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd) {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.add('active')
+        }else{
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+        }
+    }
+}
+
+
  /// When Scroll
 window.addEventListener('scroll', function() {
     changeHeaderWhenScroll()
     backToTop()
+    activateMenuAtCurrentSection()
 })
